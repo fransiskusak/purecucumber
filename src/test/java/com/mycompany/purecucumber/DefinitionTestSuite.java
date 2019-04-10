4b080f40f1cf60f86c42bb1fe3ae827b4ada9fc3
fransiskusak <f.a.k@mail.ugm.ac.id>
2019-04-10 15:28:06 +0700

    use default @CucumberOptions

--- src/test/java/com/mycompany/purecucumber/DefinitionTestSuite.java
+++ src/test/java/com/mycompany/purecucumber/DefinitionTestSuite.java
@@ -5,9 +5,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(glue = "test.java.com.mycompany.purecucumber",features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+//@CucumberOptions(glue = "test.java.com.mycompany.purecucumber",features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
 
-//@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
 //@CucumberOptions(glue = "smoketests.stepdefinitions", features = "classpath:features")
 
 public class DefinitionTestSuite {}
