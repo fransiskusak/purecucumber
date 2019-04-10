616a5231b19a0591b0102263429bd7c3d1970885
fransiskusak <f.a.k@mail.ugm.ac.id>
2019-04-10 11:38:46 +0700

    set glue in DefinitionTestSuite

--- src/test/java/com/mycompany/purecucumber/DefinitionTestSuite.java
+++ src/test/java/com/mycompany/purecucumber/DefinitionTestSuite.java
@@ -5,5 +5,8 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+@CucumberOptions(glue = "test.java.com.mycompany.purecucumber",features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+
+//@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+//@CucumberOptions(glue = "smoketests.stepdefinitions", features = "classpath:features")
 public class DefinitionTestSuite {}
